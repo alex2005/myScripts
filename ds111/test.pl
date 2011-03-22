0c8441579ac44ca87a81840fdfab2abff8141a31
Markus <markus.dorn@gmx.de>
2011-03-22 08:54:21 +0100

    Cleanup

--- ds111/test.pl
+++ ds111/test.pl
@@ -5,16 +5,16 @@ use strict;
 require File::Spec::Unix; # Done automatically by File::Spec
 
 my $escapedir;
-my $debug = 1;
+my $debug = 0;
 
 # von welchen verzeichnissen
-#my @names = qw(/volume1/music/specials/ /volume1/music/mp31/ /volume1/music/mp32/ /volume1/music/mp33/);
-#my @names = qw(/data/bck/testmp3/mp31/ /data/bck/testmp3/mp32/);
-my @names = qw(/home/dorn/mp3/jd/);
+#my @names = qw(/volume1/music/specials/ /volume1/music/mp31/ /volume1/music/mp32/ /volume1/music/mp33/) /volume1/music/mp34/;
+my @names = qw(/data/bck/testmp3/mp31/ /data/bck/testmp3/mp32/);
+#my @names = qw(/home/dorn/mp3/jd/);
 # wohin
 #my $zieldir = "/volume1/s100/";
-#my $zieldir = "/data/bck/s100/";
-my $zieldir = "/home/dorn/temp/s100/";
+my $zieldir = "/data/bck/s100/";
+#my $zieldir = "/home/dorn/temp/s100/";
 # nicht diese verzeichnisse teilnamen gehen auch!
 my @ddir = qw(@eaDir hoerspiele karaoke benjamin_bluemchen wii);
 
@@ -36,6 +36,8 @@ sub mknicename
 sub escape {
   my($str) = splice(@_);
   $str =~ s/\//\\\//g;
+  $str =~ s/\(/\\\(/g;
+  $str =~ s/\)/\\\)/g;
   $str =~ s/\@/\\\@/g;
   return $str;
 }
@@ -63,36 +65,42 @@ sub recurse {
   ## loop through the files contained in the directory
   for my $eachFile (glob($path.'*')) {
 
+      if ( $debug) { 
+      	print "# eachFile \"".$eachFile."\"\n";
+	} 
+
     ## if the file is a directory
     if( -d $eachFile and not ( inarray($eachFile) ) ) {
       ## pass the directory to the routine &( recursion )
+
+      if ( $debug) { 
+      	print "## isDir \"".$eachFile."\"\n";
+	} 
 
       my @newdir = split (/$escapedir/, $eachFile);
       my $tmpdirname = mknicename($newdir[1]);
       
       if ( $debug) { 
-      	print "### mkdir \"".$zieldir.$tmpdirname."\"\n";
+      	print "## mkdir \"".$zieldir.$tmpdirname."\"\n";
 	} else {
       	my $mk_dir = qx(mkdir -p "$zieldir$tmpdirname");
 	}
       recurse($eachFile, $newdir[1]);
     } else {
 	    if( not ( inarray($eachFile) ) and ( $eachFile =~ m/\.(mp3|MP3|ogg|jpg)/ ) ) {
+      if ( $debug) { 
+      	print "### isFile \"".$eachFile."\"\n";
+	} 
 
 	      ## print the file ... tabbed for readability
-      		my @newdir = split (/$escapedir/, $eachFile);
-      		my @filename = split (/$pdir\//, $newdir[1]);
-		
-	      my $lnsource = $pdir."\/".$filename[1];
 	      my ($volume,$directories,$fileX) = File::Spec::Unix->splitpath( $eachFile );
-	      my @path = split (/\/$fileX/, $newdir[1]);
-	      my $lndest = $zieldir.mknicename($path[0])."/".$fileX;
-	      #my $lndest = $zieldir.$filename[1];
+	      my $lnsource = $pdir."\/".$fileX;
+	      my $lndest = $zieldir.mknicename($pdir)."/".$fileX;
 	      my $rel_path = File::Spec::Unix->abs2rel( $names[$n], $pdir ) ;
 	      if ( $debug) { 
-	        #print "###\n\teachFile: $eachFile\n\tFilename: $filename[1]\n\tnewdir[1]: $newdir[1]\n";
-	        #print "\tvolume: $volume\n\tdirs: $directories\n\tfile: $path[0]\n\trepath: $rel_path\n";
-		print "\tln -s \"$rel_path/$lnsource\" \"$lndest\"\n";
+	        print "### Filename: $fileX\n";
+	        print "### edir: $names[$n]\n### dirs: $directories\n### path: $pdir\n### relpath: $rel_path\n";
+		print "### ln -s \"$rel_path/$lnsource\" \"$lndest\"\n";
 		} else {
       		my $mk_ln = qx(ln -s "$rel_path/$lnsource" "$lndest"); 
 		}
