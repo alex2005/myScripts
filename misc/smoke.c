d4b75769f6d85461eb334330e6bde25489f674e0
Markus <markus.dorn@gmx.de>
2011-12-19 13:28:38 +0100

    Added version flag

--- misc/smoke.c
+++ misc/smoke.c
@@ -2,8 +2,43 @@
 #include <stdlib.h>
 #include <time.h>
 
-main()
+char *version = "1.1";
+char *datum = "26. Juni 2000,Montags";
+void usage(void)
 {
+	printf("Usage:\n");
+	printf("Version: -v\n");
+	printf("Startdatum:  -s\n");
+	//printf(" -d<name>\n");
+	exit (8);
+}
+
+int main(int argc, char *argv[])
+{
+	//printf("Program name: %s\n", argv[0]);
+
+	while ((argc > 1) && (argv[1][0] == '-'))
+	{
+		switch (argv[1][1])
+		{
+			case 'v':
+				printf("Version %s\n", version);
+				exit (1);
+				break;
+			case 's':
+				printf("Startdatum %s\n", datum);
+				exit (1);
+				break;
+
+			default:
+				printf("Wrong Argument: %s\n", argv[1]);
+				usage();
+		}
+
+		++argv;
+		--argc;
+	}
+
 char *str, help[20];
 long value1 ,value = 10;
 
@@ -29,6 +64,8 @@ sprintf(help, "%ld", value);
 //strcat (str, help);
 printf("\nJetzt rauche ich schon %s Tage nicht mehr!\n\n", help);
 //printf("Mal schauen: %s Tage \n%s\n", help, str);
+
+return (0);
 
 }
 
